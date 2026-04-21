007a65aa7dec1685d26502f00a31347e610fb5fe
Vivian <vivianprofissi7800@gmail.com>
2026-04-21 20:33:52 -0300

    Colocando o api-gateway para funcionar.

---
+++ api-gateway/src/main/java/org/com/programming/application/ApplicationGatewayService.java
@@ -0,0 +1,11 @@
+package org.com.programming.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationGatewayService {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationGatewayService.class, args);
+    }
+}
